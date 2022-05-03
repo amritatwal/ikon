f30f3dc2a9d3396ced69d556f6580f860f4100fd
Amrit Atwal <amritatwal04@gmail.com>
2022-05-03 10:53:38 +0100

    Removed unnecessary files and started boilerplate for example icon component

---
+++ src/components/exampleIcon.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+/*
+*
+*
+*/
+
+interface IconProps {
+  placeholder: string,
+}
+
+export const exampleIcon: React.FC<IconProps> = ({ placeholder }) => {
+  return (<>
+    
+  </>);
+}
+
+
